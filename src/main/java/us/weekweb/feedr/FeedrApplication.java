4ad19152f6e83a21d2119986147b9e0f0699a9b8
joeySchentrup <j03y@live.com>
2018-10-11 12:35:30 -0400

    Init Commit of empty Spring Project

---
+++ src/main/java/us/weekweb/feedr/FeedrApplication.java
@@ -0,0 +1,12 @@
+package us.weekweb.feedr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedrApplication.class, args);
+    }
+}
