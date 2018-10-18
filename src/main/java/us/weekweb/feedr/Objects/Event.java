94a01bdd701c2659118f84e6d2982f3d58653ad2
joeySchentrup <j03y@live.com>
2018-10-18 10:25:38 -0400

    Added geter's and setter's for Event object
    * Added getter and setter for every part of object execpt owner
    
    * Owner must be set at event creation and cannot be changed

--- src/main/java/us/weekweb/feedr/Objects/Event.java
+++ src/main/java/us/weekweb/feedr/Objects/Event.java
@@ -11,12 +11,52 @@ public class Event {
 
     String name;
     String location;
-    Date time;
+    Date timeOfEvent;
     String description;
     FoodType typeOfFood;
     User owner;
 
-    public Event () {
+    public Event (User owner) {
+        this.owner = owner;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getTimeOfEvent() {
+        return timeOfEvent;
+    }
+
+    public void setTimeOfEvent(Date timeOfEvent) {
+        this.timeOfEvent = timeOfEvent;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public FoodType getTypeOfFood() {
+        return typeOfFood;
+    }
+
+    public void setTypeOfFood(FoodType typeOfFood) {
+        this.typeOfFood = typeOfFood;
     }
 }
