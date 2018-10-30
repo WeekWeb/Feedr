93d6943fd0f78bd0cd806a222bbad894b50163d3
Christopher Easton <ceaston7@hotmail.com>
2018-10-30 13:39:14 -0400

    Implemented search bar filtering

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContentMainComponent } from './content-main/content-main.component';
 import { LowbarMainComponent } from './lowbar-main/lowbar-main.component';
 import { TopbarMainComponent } from './topbar-main/topbar-main.component';
 import { SidebarMainComponent } from './sidebar-main/sidebar-main.component';
+import { SearchBarFilterPipe } from './search-bar-filter.pipe';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { SidebarMainComponent } from './sidebar-main/sidebar-main.component';
     ContentMainComponent,
     LowbarMainComponent,
     TopbarMainComponent,
-    SidebarMainComponent
+    SidebarMainComponent,
+    SearchBarFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
