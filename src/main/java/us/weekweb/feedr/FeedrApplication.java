80e01797de6197a05a47b86d27f2e927f4b4fd79
joeySchentrup <j03y@live.com>
2018-11-20 09:51:42 -0500

    Trying to deploy app

--- src/main/java/us/weekweb/feedr/FeedrApplication.java
+++ src/main/java/us/weekweb/feedr/FeedrApplication.java
@@ -1,16 +1,19 @@
 package us.weekweb.feedr;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/* intelliJ is saying to change to this notation to import SpringApplication
-   and to deprecate the use of SpringApplication.run(...) */
-import static org.springframework.boot.SpringApplication.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FeedrApplication {
+public class FeedrApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FeedrApplication.class);
+    }
 
     public static void main(String[] args) {
-        /* Check import static note for clarification of run(...)
-           without SpringApplication */
-        run(FeedrApplication.class, args);
+        SpringApplication.run(FeedrApplication.class, args);
     }
 }
