e03529dda6adca67cf8ddb09ef95abbcc795de9c
joeySchentrup <j03y@live.com>
2018-11-20 14:20:00 -0500

    Can now serve static files to frontend
    * App deployed at https://feedr-222614.appspot.com/home
    
    * Frontend needs its urls updated to not include "localhost"

--- src/main/java/us/weekweb/feedr/Controllers/HomeController.java
+++ src/main/java/us/weekweb/feedr/Controllers/HomeController.java
@@ -1,17 +1,22 @@
 package us.weekweb.feedr.Controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
+import us.weekweb.feedr.Objects.Event;
+import us.weekweb.feedr.Objects.Token;
+import us.weekweb.feedr.Services.MongoEventService;
+import us.weekweb.feedr.Services.MongoTokenService;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
-    public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
-        return modelAndView;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String redirectEverythingElseHome(){
+        return "redirect:/index.html";
     }
+
 }
